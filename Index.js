597f1ae285d0b753c90710406cf1c9cd9fbcfae6
GarrettHFullstack <garrett.hall@ttu.edu>
2022-10-04 16:38:36 -0500

    expiramental save

--- Index.js
+++ Index.js
@@ -1,15 +1,13 @@
 let board = [
-    ["x","x","x","x","x","x","x","x","x"],
-    ["x",0,0,0,0,0,0,0,"x"],
-    ["x",0,0,0,0,0,0,0,"x"],
-    ["x",0,0,0,0,0,0,0,"x"],
-    ["x",0,0,0,0,0,0,0,"x"],
-    ["x",0,0,0,0,0,0,0,"x"],
-    ["x",0,0,0,0,0,0,0,"x"],
-    ["x",0,0,0,0,0,0,0,"x"],
-    ["x","x","x","x","x","x","x","x","x"]
+    ["x","x","x","x","x"],
+    ["x",0,0,0,"x"],
+    ["x",0,0,0,"x"],
+    ["x",0,0,0,"x"],
+    ["x","x","x","x","x"]
+
 ];
 let WinStatus = false;
+let RemainingSquares = 0;
 let Player1 = ""
 let Player2 = ""
 let Player1Name = document.getElementsByClassName("players")[0]
@@ -34,21 +32,14 @@ container.addEventListener("click", function(event){
     let column = [Number(Identifier.slice(7))]
     checker1 = board[row[0]][column[0]]
     console.log("you selected "+row[0] + ","+column[0])
-    let i = 0;
-    if(checker1 == 0 && WinStatus == false){
-        while(board[row[0]+i][column[0]] == 0){
-            if(board[row[0]+i][column[0]] ==! "x"){
-            i++
-            }
-        }
-    let PaintBoard = document.getElementById(`row${row[0]+i-1}box${column[0]}`)
-    
-    board[row[0]+i-1][column[0]] = turn;
+    if(checker1 == 0 && WinStatus == false && RemainingSquares < 9){
+    let PaintBoard = document.getElementById(`row${row[0]}box${column[0]}`)
+    board[row[0]][column[0]] = turn;
     console.log(board)
-    diagonal1(row[0]+i-1,column[0])
-    diagonal2(row[0]+i-1,column[0])
-    topcheck(row[0]+i-1,column[0])
-    RightLeft(row[0]+i-1,column[0])
+    diagonal1(row[0],column[0])
+    diagonal2(row[0],column[0])
+    topcheck(row[0],column[0])
+    RightLeft(row[0],column[0])
     if(turn == Player1){
         PaintBoard.classList.add("Player1")
         if(WinStatus == false){
@@ -57,25 +48,26 @@ container.addEventListener("click", function(event){
     } else {
         PaintBoard.classList.add("Player2")
         if(WinStatus == false){
-        turn = Player1  
+        turn = Player1 
         } 
     }
     } else if(WinStatus == true){
         alert(turn + " Has won the game, please restart")
+    } else if(RemainingSquares == 10){
+        alert("You have Tied, please restart the game")
     }
     else{
         alert("square is taken please choose a different square")
     }
-    console.log(document.getElementById(`row${row[0]+i-1}box${column[0]}`))
 })
 function creatediv(){
-    for(let y = 1; y <= 7; y++){
+    for(let y = 1; y <= 3; y++){
         let row = document.createElement('div')
         row.classList.add("row");
         row.id = `row${y}`
         container.append(row);
         row = document.getElementById(`row${y}`)
-        for(let l = 1; l < 8; l++){
+        for(let l = 1; l < 4; l++){
             let box = document.createElement('div');
             box.id = `row${y}box${l}`
             box.classList.add ("box")
@@ -89,8 +81,6 @@ function diagonal1(row,column){
     console.log("your column is" +column)
     let k = []
     let i = 1;
-    console.log(row - i)
-    console.log(column + i)
     while(board[row+i][column-i] == turn){
         k.push(board[row+i][column-i])
         console.log("while 1" + k)
@@ -102,18 +92,14 @@ function diagonal1(row,column){
         console.log("while 1" + k)
         i++;
     }
-    console.log(k)
-    if(k.length >= 3){
+    console.log("Diagonal1:" + k)
+    if(k.length >= 2){
         Winner();
     }
 }
 function diagonal2(row,column){
-    console.log("D2your row is " + row)
-    console.log("D2your column is " +column)
     let k = []
     let i = 1;
-    console.log(row - i)
-    console.log(column + i)
     while(board[row-i][column-i] == turn){
         k.push(board[row-i][column-i])
         console.log("while 1" + k)
@@ -125,35 +111,33 @@ function diagonal2(row,column){
         console.log("while 1" + k)
         i++;
     }
-    console.log(k)
-    if(k.length >= 3){
+    console.log("Diagonal2:" + k)
+    if(k.length >= 2){
         Winner();
     }
 }
 function topcheck(row,column){
-    console.log("D2your row is " + row)
-    console.log("D2your column is " +column)
     let k = []
     let i = 1;
-    console.log(row - i)
-    console.log(column + i)
     while(board[row+i][column] == turn){
         k.push(board[row+i][column])
         console.log("while 1" + k)
         i++;
     }
-    console.log(k)
-    if(k.length >= 3){
+    i = 1;
+    while(board[row-i][column] == turn){
+        k.push(board[row-i][column])
+        console.log("while 1" + k)
+        i++;
+    }
+    console.log("TopCheck:" + k)
+    if(k.length >= 2){
         Winner();
     }
 }
 function RightLeft(row,column){
-    console.log("D2your row is " + row)
-    console.log("D2your column is " +column)
     let k = []
     let i = 1;
-    console.log(row - i)
-    console.log(column + i)
     while(board[row][column-i] == turn){
         k.push(board[row][column-i])
         console.log("while 1" + k)
@@ -165,12 +149,16 @@ function RightLeft(row,column){
         console.log("while 1" + k)
         i++;
     }
-    console.log(k)
-    if(k.length >= 3){
+    
+    console.log("LeftRight:" + k)
+    if(k.length >= 2){
         Winner();
+    } 
+    RemainingSquares++;
+    if(RemainingSquares == 9){
+        alert("You have Tied, please restart the game")
     }
 }
-
 creatediv();
 function Winner(){
     WinStatus = true;
