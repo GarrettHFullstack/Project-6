9765850aa96af1c9694cf489bd4e96e385fdd6c5
GarrettHFullstack <garrett.hall@ttu.edu>
2022-10-05 21:40:43 -0500

    cheezy

--- Index.js
+++ Index.js
@@ -6,58 +6,66 @@ let board = [
     ["x","x","x","x","x"]
 
 ];
+
 let WinStatus = false;
 let RemainingSquares = 0;
 let Player1 = ""
 let Player2 = ""
 let Player1Name = document.getElementsByClassName("players")[0]
 let Player2Name = document.getElementsByClassName("players")[1]
-while(Player2 == "" || Player1 == Player2){
+getnames();
+while(Player2 == "" || Player1 == Player2 || Player1 == ""){
     if (Player1 == ""){
-    Player1 = prompt("what is playing 1 name?")
-    Player1Name.textContent = Player1
+    Player1 = prompt("what is playing 1 name?").toUpperCase()
+    Player1Name.textContent = "Player 1: " + Player1
 }   if (Player2 == "" || Player1 == Player2){
-    Player2 = prompt("what is playing 2 name?")
-    Player2Name.textContent = Player2
+    Player2 = prompt("what is playing 2 name?").toUpperCase()
+    Player2Name.textContent = "Player 2: " + Player2
 }   if (Player1 == Player2){
     alert("Please Use a different name")
 }
 }
 let turn = Player1;
 let container = document.getElementById("board");
+let PaintBoard = "";
 container.addEventListener("click", function(event){
     let Identifier = event.target.id;
-    console.log(Identifier)
+    let Identifier2 = event.target;
+    console.log(Identifier2)
     let row = [Number(Identifier.slice(3,4))]
     let column = [Number(Identifier.slice(7))]
     checker1 = board[row[0]][column[0]]
-    console.log("you selected "+row[0] + ","+column[0])
-    if(checker1 == 0 && WinStatus == false && RemainingSquares < 9){
-    let PaintBoard = document.getElementById(`row${row[0]}box${column[0]}`)
+    if(checker1 == 0 && WinStatus == false && RemainingSquares < 9 && Identifier2.classList.contains("box")==true){
+    PaintBoard = document.getElementById(`row${row[0]}box${column[0]}`)
     board[row[0]][column[0]] = turn;
-    console.log(board)
+    if(turn == Player1){
+        PaintBoard.classList.add("Player1")
+    }else {
+        PaintBoard.classList.add("Player2")
+    }
     diagonal1(row[0],column[0])
     diagonal2(row[0],column[0])
     topcheck(row[0],column[0])
     RightLeft(row[0],column[0])
+    ai();
     if(turn == Player1){
-        PaintBoard.classList.add("Player1")
         if(WinStatus == false){
         turn = Player2
     }
-    } else {
-        PaintBoard.classList.add("Player2")
+    } else {  
         if(WinStatus == false){
         turn = Player1 
         } 
     }
     } else if(WinStatus == true){
         alert(turn + " Has won the game, please restart")
-    } else if(RemainingSquares == 10){
+    } else if(RemainingSquares == 9){
         alert("You have Tied, please restart the game")
     }
     else{
+        if(Identifier2.classList.contains("box")==true){
         alert("square is taken please choose a different square")
+        }
     }
 })
 function creatediv(){
@@ -77,22 +85,20 @@ function creatediv(){
     }     
 }
 function diagonal1(row,column){
-    console.log("your row is" + row)
-    console.log("your column is" +column)
     let k = []
     let i = 1;
     while(board[row+i][column-i] == turn){
         k.push(board[row+i][column-i])
-        console.log("while 1" + k)
+
         i++;
     }
     i = 1;
     while(board[row-i][column+i] == turn){
         k.push(board[row-i][column+i])
-        console.log("while 1" + k)
+
         i++;
     }
-    console.log("Diagonal1:" + k)
+
     if(k.length >= 2){
         Winner();
     }
@@ -102,16 +108,14 @@ function diagonal2(row,column){
     let i = 1;
     while(board[row-i][column-i] == turn){
         k.push(board[row-i][column-i])
-        console.log("while 1" + k)
+
         i++;
     }
     i = 1;
     while(board[row+i][column+i] == turn){
         k.push(board[row+i][column+i])
-        console.log("while 1" + k)
         i++;
     }
-    console.log("Diagonal2:" + k)
     if(k.length >= 2){
         Winner();
     }
@@ -121,16 +125,13 @@ function topcheck(row,column){
     let i = 1;
     while(board[row+i][column] == turn){
         k.push(board[row+i][column])
-        console.log("while 1" + k)
         i++;
     }
     i = 1;
     while(board[row-i][column] == turn){
         k.push(board[row-i][column])
-        console.log("while 1" + k)
         i++;
     }
-    console.log("TopCheck:" + k)
     if(k.length >= 2){
         Winner();
     }
@@ -140,27 +141,199 @@ function RightLeft(row,column){
     let i = 1;
     while(board[row][column-i] == turn){
         k.push(board[row][column-i])
-        console.log("while 1" + k)
         i++;
     }
     i = 1;
     while(board[row][column+i] == turn){
         k.push(board[row][column+i])
-        console.log("while 1" + k)
         i++;
     }
     
-    console.log("LeftRight:" + k)
+
     if(k.length >= 2){
         Winner();
     } 
     RemainingSquares++;
-    if(RemainingSquares == 9){
-        alert("You have Tied, please restart the game")
-    }
 }
 creatediv();
 function Winner(){
+   
     WinStatus = true;
-    console.log("Contrats" + turn + "you are the victor!!!!!")
+
+    alert(turn + " Has won the game lolG")
+}
+let ClearButton = document.getElementById("clearResults");
+
+ClearButton.addEventListener("click", clearboard)
+function clearboard(){
+    for (let p = 1; p < 4; p++){
+        for (let k = 1; k < 4; k++){
+            let Ind = document.getElementById(`row${p}box${k}`)
+            Ind.classList.remove('Player1')
+            Ind.classList.remove('Player2')
+            RemainingSquares = 0;
+            board[p][k] = 0;
+            WinStatus = false
+        }
+    }
+    console.log(board)
+}
+
+function ai(){
+    let Holder = {
+    
+    }
+    let greatest = 0;
+    console.log(board)
+    for(let RowInt = 1; RowInt <=3; RowInt++){
+        for(let ColumnInt = 1; ColumnInt <=3; ColumnInt++){
+            if(board[RowInt][ColumnInt] == 0){
+                Holder[`row${RowInt}column${ColumnInt}`] = diagonal1Ai(RowInt,ColumnInt)+diagonal2Ai(RowInt,ColumnInt)+topcheckAi(RowInt,ColumnInt)+RightLeftAi(RowInt,ColumnInt)
+                if(diagonal1Ai(RowInt,ColumnInt)+diagonal2Ai(RowInt,ColumnInt)+topcheckAi(RowInt,ColumnInt)+RightLeftAi(RowInt,ColumnInt)> greatest){
+                    greatest = diagonal1Ai(RowInt,ColumnInt)+diagonal2Ai(RowInt,ColumnInt)+topcheckAi(RowInt,ColumnInt)+RightLeftAi(RowInt,ColumnInt);
+                }
+            }
+        }
+        
+    }
+    console.log(Holder)
+    console.log(greatest)
+}
+function diagonal1Ai(row1,column1){
+    let k = 0;
+    let inRow = []
+    let i = 1;
+    while(board[row1+i][column1-i] !== "x"){
+        if(board[row1+i][column1-i] == Player1){
+            k++
+            inRow.push(1)
+        }
+        if(board[row1+i][column1-i] == "AI"){
+            k--
+        }
+        i++;
+    }
+    i = 1;
+    while(board[row1-i][column1+i] !== "x"){
+        if(board[row1-i][column1+i] == Player1){
+            k++
+            inRow.push(1)
+        }
+        if(board[row1-i][column1+i] == "AI"){
+            k--
+        }
+        i++;
+    }
+    if(inRow.length == 2){
+        k = k+5;
+    }
+    return k;
+}
+//Diagonml1Ai goes from top left to bottom right
+function diagonal2Ai(row1,column1){
+    let k = 0;
+    let i = 1;
+    let inRow = []
+    console.log(turn)
+    while(board[row1-i][column1-i] !== "x"){
+       if(board[row1-i][column1-i] == Player1){
+        k++
+        inRow.push(1)
+       }
+       if(board[row1-i][column1-i] == "AI"){
+        k--
+       }
+
+        i++;
+    }
+    i = 1;
+    while(board[row1+i][column1+i] !== "x"){
+       if(board[row1+i][column1+i]== Player1){
+        k++
+        inRow.push(1)
+       }
+       if(board[row1+i][column1+i]== "AI"){
+        k--
+       }
+        i++;
+    }
+    if(inRow.length == 2){
+        k = k+5;
+    }
+    return k;
+}
+function topcheckAi(row1,column1){
+    let k = 0
+    let i = 1;
+    let inRow = []
+    while(board[row1+i][column1] !== "x"){
+        if(board[row1+i][column1]== Player1){
+            k++
+            inRow.push(1)
+        }
+        if(board[row1+i][column1]== "AI"){
+        k--
+        }
+        i++;
+    }
+    if(inRow.length == 2){
+        k = k+5;
+    }
+    i = 1;
+    while(board[row1-i][column1] !== "x"){
+       if(board[row1-i][column1] == Player1){
+            k++
+            inRow.push(1)
+       }
+       if(board[row1-i][column1] == "AI"){
+            k--
+   }
+        i++;
+        }
+        if(inRow.length == 2){
+            k = k+5;
+        }
+    return k;
+
+}
+function RightLeftAi(row1,column1){
+    let k = 0
+    let i = 1;
+    let inRow = []
+    console.log("checking object" + row1 + column1)
+    while(board[row1][column1-i] !== "x"){
+        if(board[row1][column1-i] == Player1){
+            k++
+            inRow.push(1)
+        }
+        if(board[row1][column1-i] == "AI"){
+            k--
+        }
+        i++;
+    }
+    i = 1;
+    while(board[row1][column1+i] !== "x"){
+        if(board[row1][column1+i] == Player1){
+            k++
+            inRow.push(1)
+        }
+        if(board[row1][column1+i] == "AI"){
+            k--
+        }
+        i++;
+    }
+        if(inRow.length == 2){
+            k = k+5;
+        }
+    return k;
+}
+function getnames(){
+    let x = document.createElement("div")
+    x.classList.add("startdiv")
+    console.log(x)
+    let getNamesOverlay = 
+    `<div id = "getNames" style = "display: flex;background-color = white; align-items: center;position: absolute; flex-direction: column;justify-content: center;width: 100%; height: 100%"><div class = "upper">WELCOME TO GARRETTS<br>MILK-TAK-TOE</div><div class = "lower"><div class = "PlayerText">Please enter Player 1's Name</div> <input class = "startInput" type = "text"><button class = "submit" type = "submit" >Submit</button><div></div>`
+    x.innerHTML = getNamesOverlay;
+    let startpage = document.getElementsByTagName("body")[0]
+    startpage.prepend(x)
 }
